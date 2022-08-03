75e18364b056b1fe29e44b0cc2b9df76fcf460f8
WonGyu <bitamin707@naver.com>
2022-08-03 16:45:50 +0900

    축제 페이지 DB 연결

---
+++ VISIT_JEJU/src/main/java/com/human/dto/festival/festivalDto.java
@@ -0,0 +1,62 @@
+package com.human.dto.festival;
+
+import java.sql.Date;
+
+public class festivalDto {
+	private Integer fno;
+	private String fname;
+	private String fcontent;
+	private Date fstart_date;
+	private Date fend_date;
+	private String flocation;
+	private String fregion;
+	
+	public Integer getFno() {
+		return fno;
+	}
+	public void setFno(Integer fno) {
+		this.fno = fno;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getFcontent() {
+		return fcontent;
+	}
+	public void setFcontent(String fcontent) {
+		this.fcontent = fcontent;
+	}
+	public Date getFstart_date() {
+		return fstart_date;
+	}
+	public void setFstart_date(Date fstart_date) {
+		this.fstart_date = fstart_date;
+	}
+	public Date getFend_date() {
+		return fend_date;
+	}
+	public void setFend_date(Date fend_date) {
+		this.fend_date = fend_date;
+	}
+	public String getFlocation() {
+		return flocation;
+	}
+	public void setFlocation(String flocation) {
+		this.flocation = flocation;
+	}
+	public String getFregion() {
+		return fregion;
+	}
+	public void setFregion(String fregion) {
+		this.fregion = fregion;
+	}
+	
+	@Override
+	public String toString() {
+		return "festivalDao [fno=" + fno + ", fname=" + fname + ", fcontent=" + fcontent + ", fstart_date="
+				+ fstart_date + ", fend_date=" + fend_date + ", flocation=" + flocation + ", fregion=" + fregion + "]";
+	}
+}
