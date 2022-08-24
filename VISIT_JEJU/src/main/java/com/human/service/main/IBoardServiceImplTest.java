0a187d16427243fd708de1b43d884c53a2fce356
noooooong <noooooong@naver.com>
2022-08-24 17:20:23 +0900

    인공지능 추천기능 탑재

--- VISIT_JEJU/src/main/java/com/human/service/main/IBoardServiceImplTest.java
+++ VISIT_JEJU/src/main/java/com/human/service/main/IBoardServiceImplTest.java
@@ -21,52 +21,21 @@ public class IBoardServiceImplTest implements IBoardServiceTest {
 		dao.createTest(dto);
 	}
 
-	@Override
-	public BoardDtoTest springTest(BoardDtoTest dto) throws Exception {
-		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
-		BoardDtoTest r=dao.springTest(dto);
-		return r;
-	}
-
-	@Override
-	public BoardDtoTest summerTest(BoardDtoTest dto) throws Exception {
-		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
-		BoardDtoTest r=dao.summerTest(dto);
-		return r;
-	}
-
-	@Override
-	public BoardDtoTest autumnTest(BoardDtoTest dto) throws Exception {
-		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
-		BoardDtoTest r=dao.autumnTest(dto);
-		return r;
-	}
-
-	@Override
-	public BoardDtoTest winterTest(BoardDtoTest dto) throws Exception {
-		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
-		BoardDtoTest r=dao.winterTest(dto);
-		return r;
-	}
-
-	@Override
-	public BoardDtoTest maleTest(BoardDtoTest dto) throws Exception {
-		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
-		BoardDtoTest r=dao.maleTest(dto);
-		return r;
-	}
-
-	@Override
-	public BoardDtoTest femaleTest(BoardDtoTest dto) throws Exception {
-		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
-		BoardDtoTest r=dao.femaleTest(dto);
-		return r;
-	}
+	
 
 	@Override
 	public List<BoardDtoTest> listAllTest() throws Exception {
 		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
 		List<BoardDtoTest> dtos=dao.listAllTest();
+		return dtos;
+	}
+
+
+
+	@Override
+	public List<BoardDtoTest> readTest(String title) throws Exception {
+		IBoardDaoTest dao=sqlSession.getMapper(IBoardDaoTest.class);
+		List<BoardDtoTest> dtos=dao.readTest(title);
 		return dtos;
 	}
 
--- VISIT_JEJU/src/main/webapp/WEB-INF/views/main/main.jsp
+++ VISIT_JEJU/src/main/webapp/WEB-INF/views/main/main.jsp
@@ -56,36 +56,6 @@ window.onload=function(){
 	});
 }
 
-let slideIndex = 2;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-let i;
-let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("dot");
-if (n > slides.length) {
-    slideIndex = 1
-}    
-if (n < 1) {
-    slideIndex = slides.length
-}
-for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-}
-for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-}
-slides[slideIndex-1].style.display = "block";  
-dots[slideIndex-1].className += " active";
-}
 
 </script>
 </head>
@@ -329,6 +299,38 @@ var check = "${Check}"
 	}else {
 		document.getElementById("remocon").style.display = "none";	
 	}
+	
+let slideIndex = 2;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {
+    slideIndex = 1
+}    
+if (n < 1) {
+    slideIndex = slides.length
+}
+for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+}
+
 </script>
 
     
